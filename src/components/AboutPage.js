abf5d512b3e754afe2ec3bd690bbbd2abd35582b
Apiyo4 <odingoannette@gmail.com>
2019-11-19 18:52:34 +0300

    Finish on IssueForm

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Intro from './Intro';
-import IssueForm from './IssueForm'
+import IssueForm from './IssueForm';
+import Styled from 'styled-components'
 
+const Div1 = Styled.div`
+   
+    text-align: center;
+`
 function AboutPage(){
     return(
         <div>
             <Intro />
+            <Div1>
+                <h2>Post an Issue</h2>
             <IssueForm />
+
+            </Div1>
             
         </div>
 
--- src/components/IssueForm.js
+++ src/components/IssueForm.js
@@ -1,8 +1,97 @@
 import React from 'react';
+import {withFormik, Form, Field, ErrorMessage} from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+import Styled from 'styled-components';
+
+const Div = Styled.div`
+    width: 30%;
+    margin: auto;
+`
+const Span= Styled.span`
+    padding-right: 5px;
+`
+const Span1= Styled.span`
+    padding-right: 5px;
+  
+    
+`
+const Button = Styled.button`
+    color: #EEEEEE;
+    background-color: #2723EC;
+    width: 89px;
+    height: 21 px;
+    text-align: center;
+    border-radius: 10% / 50%;;
+   
+    margin-left: 30%;
+    margin-right: 30%;
+`
 
 function IssueForm(){
     return(
-        <h1>IssueForm</h1>
+        <Div>
+        <Form>
+            <ErrorMessage name='description' render={msg => <div className="error">{msg}</div>} />
+            <label> <Span1> Description:</Span1>
+                <Field component={()=><textarea type='textarea' name='description' placeholder='Enter description' cols='20' rows= '1'></textarea>}
+                
+               />
+            </label>
+            <br />
+            <ErrorMessage name='latitude' render={msg => <div className="error">{msg}</div>} />
+            <label> <Span>Latitude:</Span>
+                <Field type='text' name='latitude' placeholder='Enter latitude'/>
+            </label>
+            <br />
+            <ErrorMessage name='longitude' render={msg => <div className="error">{msg}</div>} />
+            <label> <Span>Longitude:</Span> 
+                <Field type='text' name='longitude' placeholder='Enter longitude'/>
+            </label>
+            <br />
+            <ErrorMessage name='username' render={msg => <div className="error">{msg}</div>} />
+            <label> <Span>Username:</Span>
+                <Field type='text' name='username' placeholder='Enter username'/>
+            </label>
+            <br />
+            <ErrorMessage name='imageUrl' render={msg => <div className="error">{msg}</div>} />
+            <label> <Span>ImageUrl:</Span>
+                <Field type='text' name='imgUrl' placeholder='Enter image url'/>
+            </label>
+            <br />
+            
+            <Button type='submit'>Submit</Button>
+        </Form>
+        </Div>
     )
 }
-export default IssueForm;
+const IssueFormWithFormik = withFormik(
+    {
+        mapPropsToValues(){
+            return{
+                description: '',
+                latitude: '',
+                longitude: '',
+                username: '',
+                imageUrl: ''
+            }
+        },
+        validationSchema:Yup.object().shape({
+            description:  Yup.string().min(5, 'Too Short!').max(200, 'Too Long!').required("Please enter a description"),
+            latiitude:  Yup.string().required("Please enter latitude for the area you're in"),
+            longitude:  Yup.string().required("Please enter longitude for the area you're in"),
+            username:  Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required("Please enter username"),
+            imageUrl: Yup.string().url().required('Please enter an image url')
+        }),
+        handleSubmit(values, tools){
+            axios.post('https://comake-simple.herokuapp.com/api/issues', values)
+            .then(response=>{
+                tools.resetForm();
+            })
+            .catch(error=>{
+                console.log(error);
+            })
+        }
+    }
+)(IssueForm)
+export default IssueFormWithFormik;
