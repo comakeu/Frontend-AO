a1369b3cbbc837c4c60451f07b150e10de32901e
josiah <josiahdamiwilliams@gmail.com>
2019-11-19 17:42:16 +0100

    API done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import HomePage from './components/HomePage';
-import {Route} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+// // // import Header from './components/Header';
+// // // import Footer from './components/Footer';
+// // // import HomePage from './components/HomePage';
+// // import {Route} from 'react-router-dom';
+// import API from './API';
 
+const Issues = () => {
+  const [hasError, setErrors] = useState(false)
+  const [issues, setIssues] = useState({});
 
+  async function fetchData() {
+    const res = await fetch("https://comake-simple.herokuapp.com/api/issues")
+    res
+      .json()
+      .then(res => setIssues(res))
+      .catch(err => setErrors(err));
+  }
 
-import './App.css';
+  useEffect(() => {
+    fetchData();
+  })
 
-function App() {
   return (
-    <div className="App">
-     <Header />
-     <HomePage />
-     <Footer />
-  
+    <div>
+      <span>{JSON.stringify(issues)}</span>
+      <hr />
+      <span>hasError: {JSON.stringify(hasError)}</span>
     </div>
   );
-}
+};
 
-export default App;
+
+export default Issues;
