c0a66cbb3061b101ced3ad02aedce1e52ed4a5ae
Apiyo4 <odingoannette@gmail.com>
2019-11-21 07:09:50 +0300

    Display only data return fom the filtered issue

--- src/components/IssueCard.js
+++ src/components/IssueCard.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import {useParams} from  'react-router';
+import {Link} from 'react-router-dom';
 
-function IssueCard(){
+function IssueCard(props){
     let {id} = useParams();
     const issue = props.issues.filter(issue=>issue.id.toString() ===id );    
 
     return (
-        <h1> Hello</h1>
+        <div>
+            <img src={issue[0].imgURL} alt='photo of a city'/>
+            <h3>{issue[0].username}</h3>
+            <p>Number of votes: >{issue[0].votes}</p>
+            <p>Latitude: {issue[0].latitude} </p>
+            <p>Longitude: {issue[0].longitude}</p>
+            <p>{issue[0].description}</p>
+            <p> Consider this issue urgent, give it a vote.</p>
+            <button  type='submit'>Vote</button>
+            <Link to="/about">
+            <h4>Go Back</h4>
+            </Link>
+            
+        </div>
     )
 }
 
