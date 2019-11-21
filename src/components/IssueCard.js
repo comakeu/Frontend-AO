1d48214a32d5b3be94dc48ef37871c2bc6f608f3
Apiyo4 <odingoannette@gmail.com>
2019-11-21 07:04:47 +0300

    Filter and return issue where the issue id is equal to the id declared before

--- src/components/IssueCard.js
+++ src/components/IssueCard.js
@@ -3,7 +3,7 @@ import {useParams} from  'react-router';
 
 function IssueCard(){
     let {id} = useParams();
-        
+    const issue = props.issues.filter(issue=>issue.id.toString() ===id );    
 
     return (
         <h1> Hello</h1>
